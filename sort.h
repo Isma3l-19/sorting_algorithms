a2776ed8c3d33d7837d225af9a99c3d105ee3e07
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-12-18 06:18:48 -0800

    task 4

--- sort.h
+++ sort.h
@@ -52,5 +52,4 @@ void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 int lomuto_partition(int *array, size_t size, int left, int right);
 void lomuto_sort(int *array, size_t size, int left, int right);
-void quick_sort(int *array, size_t size);
 #endif /* SORT_H */
