e07c12ccdb438b492b09391994e6edc4b42b09db
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-12-19 05:25:51 -0800

    task 9

--- sort.h
+++ sort.h
@@ -60,7 +60,8 @@ void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
 void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
 void merge_sort(int *array, size_t size);
 void max_heapify(int *array, size_t size, size_t base, size_t root);
-
+int get_max(int *array, int size);
+void radix_counting_sort(int *array, size_t size, int sig, int *buff);
 
 
 #endif /* SORT_H */
